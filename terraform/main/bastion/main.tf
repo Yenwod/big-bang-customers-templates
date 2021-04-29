29e7d810e151367e512f2fe24d8692dcd68a0b3f
Michael McLeroy <michaelmcleroy@cloudfitsoftware.com>
2021-04-29 17:24:00 -0400

    feat: add python to bastion

--- terraform/main/bastion/main.tf
+++ terraform/main/bastion/main.tf
@@ -42,6 +42,7 @@ resource "aws_launch_configuration" "bastion_lc" {
   security_groups      = ["${aws_security_group.bastion_sg.id}"]
   key_name             = "${var.key_name}"
   associate_public_ip_address = "true"
+  user_data = "${file("dependencies/install_python.sh")}"
 
   lifecycle {
     create_before_destroy = "true"
